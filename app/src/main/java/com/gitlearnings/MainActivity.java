77f51b345e3ff8caf3c459abb09bb0010185c2c5
sagarika-nagunoori <sagarika.nagunoori@gmail.com>
2016-10-26 18:51:36 +0530

    create and switc branch

--- app/src/main/java/com/gitlearnings/MainActivity.java
+++ app/src/main/java/com/gitlearnings/MainActivity.java
@@ -10,11 +10,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
         development();
     }
 
     private void development() {
         /*we are in development branch*/
+
+        /*create branch
+        * git branch branchname*/
+
+        /*switch to development branch
+        * git checkout development*/
+
+
+
+
+
     }
 }
