6edce28eaebdfa5197832ec17c667fa2af460129
sagarika-nagunoori <sagarika.nagunoori@gmail.com>
2016-10-27 16:10:49 +0530

    deleted toast

--- app/src/main/java/com/gitlearnings/MainActivity.java
+++ app/src/main/java/com/gitlearnings/MainActivity.java
@@ -33,8 +33,7 @@ public class MainActivity extends AppCompatActivity {
         /*push the project
         * git push origin branchname
         * ex:git push origin master/develolpment*/
-        Toast.makeText(this,"development",Toast.LENGTH_SHORT).show();
-        Toast.makeText(this,"development1",Toast.LENGTH_SHORT).show();
+      
 
     }
 }
